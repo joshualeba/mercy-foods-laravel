7cfb5838da97503e9d09c248e5c38a43d9d1d561
Zaulalv <123046350@upq.edu.mx>
2025-11-15 19:42:55 -0600

    HU_2.2 Completo

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             RestaurantDetailSeeder::class,
             PlatilloSeeder::class,
+            FaqSeeder::class,
         ]);
     }
 }
